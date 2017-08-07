024141d98921afbaff4012683c2c532beb4bcd62
Jayme <jaymecalixto@gmail.com>
2017-08-06 21:26:47 -0300

    window handling in browser

--- Sources/utils/Window.hx
+++ Sources/utils/Window.hx
@@ -2,7 +2,6 @@ package utils;
 
 import js.Browser;
 import js.html.CanvasElement;
-import kha.System;
 
 class Window{
 
@@ -18,6 +17,11 @@ class Window{
 
 		var c:CanvasElement = cast Browser.document.getElementById("khanvas");
 
+		c.style.paddingTop = "0px";
+		c.style.paddingLeft = "0px";
+		c.style.background = "#FF0000";
+		c.style.position = "absolute";
+
 		windowWidth = Std.int(Browser.window.innerWidth * 0.95);
 		windowHeight = Std.int(Browser.window.innerHeight * 0.95);
 
@@ -26,10 +30,9 @@ class Window{
 
 		#else
 
-		windowWidth = System.windowWidth;
-		windowHeight = System.windowHeight;
+		windowWidth = kha.System.windowWidth;
+		windowHeight = kha.System.windowHeight;
 
 		#end
 	}
-
 }
