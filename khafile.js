037244a15929a2d994ecd371ab59e51214345adc
Jayme <jaymecalixto@gmail.com>
2017-08-06 21:26:36 -0300

    better asset management

--- khafile.js
+++ khafile.js
@@ -1,4 +1,4 @@
 let project = new Project('Project caravan');
-project.addAssets('Assets/**');
+project.addAssets('Assets/**', { nameBaseDir: 'Assets', destination: '{dir}/{name}', name: '{dir}/{name}' });
 project.addSources('Sources');
 resolve(project);
