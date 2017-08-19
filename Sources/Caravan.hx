4cc13a04b99ffaf937686a3534babd4d22433ef0
jaycalixto <jaymecalixto@gmail.com>
2017-08-18 22:24:22 -0300

    crude input handling

--- Sources/Caravan.hx
+++ Sources/Caravan.hx
@@ -9,12 +9,13 @@ import kha.Scaler;
 import kha.Assets;
 import mankha.gfx.AnimatedSprite;
 import mankha.utils.Window;
+import mankha.input.InputHandler;
 
 class Caravan {
 
 	//Shift to square resolution
-	public static var gameWidth = 256;
-	public static var gameHeight = 256;
+	public static var gameWidth = 320;
+	public static var gameHeight = 192;
 
 	private static var bgColor = Color.fromValue(0x0A0A0F);
 	
@@ -23,16 +24,13 @@ class Caravan {
 	private var windowCanvas:Window;
 
 	private var spr: AnimatedSprite;
+	private var inputHandler: InputHandler;
 
 	public function new(w:Window) {
 		windowCanvas = w;
-		//windowCanvas.forceIntegerResolution(gameWidth, gameHeight);
 		
 		System.notifyOnRender(render);
 		
-		trace("w: " + windowCanvas.windowWidth + " h: " + windowCanvas.windowHeight);
-		//System.changeResolution(windowCanvas.windowWidth, windowCanvas.windowHeight);
-
 		Scheduler.addTimeTask(update, 0, 1 / 60);
 
 		backbuffer = Image.createRenderTarget(gameWidth, gameHeight);
@@ -40,6 +38,8 @@ class Caravan {
 		Assets.loadImageFromPath("char1.png", true, function(image) {
 			spr = new AnimatedSprite(image, 24, 38);
 		});
+
+		inputHandler = new InputHandler();
 	}
 
 	function update(): Void {
@@ -48,8 +48,32 @@ class Caravan {
 
 	function render(framebuffer: Framebuffer): Void {		
 		backbuffer.g2.begin(bgColor);
-		if(spr != null) {
-			//backbuffer.g2.drawImage(img, 0, 0);
+		if (spr != null) {
+
+			if (inputHandler.up) {
+				spr.position.y -= 1;
+			} else if (inputHandler.down) {
+				spr.position.y += 1;
+			}
+
+			if(inputHandler.left) {
+				spr.position.x -= 1;
+			} else if (inputHandler.right) {
+				spr.position.x += 1;
+			}
+
+			if (spr.position.x < 0) {
+				spr.position.x = 0;
+			} else if ((spr.position.x + spr.frameW) > gameWidth) {
+				spr.position.x = gameWidth - spr.frameW;
+			}
+
+			if(spr.position.y < 0) {
+				spr.position.y = 0;
+			} else if ((spr.position.y + spr.frameH) > gameHeight) {
+				spr.position.y = gameHeight - spr.frameH;
+			}
+
 			spr.draw(backbuffer.g2);
 		}
 		backbuffer.g2.end();
@@ -58,6 +82,5 @@ class Caravan {
 		Scaler.scale(backbuffer, framebuffer, System.screenRotation);
 		framebuffer.g2.end();
 
-		//trace("fb w: "+ framebuffer.width+ " fb h: " + framebuffer.height);
 	}
 }
